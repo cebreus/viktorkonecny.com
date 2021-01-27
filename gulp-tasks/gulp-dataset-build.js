c33a767990472ca9471c9f86c59786b41d8c42c0
Emil Čelustka <46893289+ecelustka@users.noreply.github.com>
2021-01-27 09:46:35 +0100

    fix: blog post sorting
    fix: blog post sorting

--- gulp-tasks/gulp-dataset-build.js
+++ gulp-tasks/gulp-dataset-build.js
@@ -3,7 +3,7 @@ const gulp = require('gulp');
 const data = require('gulp-data');
 const mergeJson = require('gulp-merge-json');
 const path = require('path');
-const plumber = require('gulp-plumber');
+const through = require('through2');
 
 /**
  * @description Merge JSON files into one; each file in new node
@@ -14,36 +14,58 @@ const plumber = require('gulp-plumber');
  */
 
 const datasetBuild = (input, output, outputFilename, cb) => {
-  return gulp
-    .src(input)
-    .pipe(
-      mergeJson({
-        fileName: outputFilename,
-        concatArrays: true,
-        mergeArrays: false,
-        edit: (json, file) => {
-          let data = {};
-          let fileName = path.parse(file.path).name;
-          let fileDir = path.parse(file.path).dir;
+  return (
+    gulp
+      .src(input)
+      .pipe(
+        mergeJson({
+          fileName: outputFilename,
+          concatArrays: true,
+          mergeArrays: false,
+          edit: (json, file) => {
+            let data = {};
+            let fileName = path.parse(file.path).name;
+            let fileDir = path.parse(file.path).dir;
 
-          if (fileName === 'site') {
-            let primaryKey = fileName;
-            data[primaryKey.toUpperCase()] = json;
-          } else if (fileDir.includes('dataset-')) {
-            let primaryKey = fileDir
-              .split(path.sep)
-              .pop()
-              .replace('_dataset-', '');
-            data[primaryKey.toUpperCase()] = [json].sort(
-              (a, b) => new Date(b['startDate']) - new Date(a['startDate'])
-            );
-          }
-          return data;
-        },
-      })
-    )
-    .pipe(gulp.dest(output))
-    .on('end', cb);
+            // site data
+            if (fileName === 'site') {
+              let primaryKey = fileName;
+              data[primaryKey.toUpperCase()] = json;
+            }
+            // datasets e.g. Blog posts
+            else if (fileDir.includes('dataset-')) {
+              let primaryKey = fileDir
+                .split(path.sep)
+                .pop()
+                .replace('_dataset-', '');
+              data[primaryKey.toUpperCase()] = [json];
+            }
+            return data;
+          },
+        })
+      )
+      // Sorting BLOG by `entity_status.date`
+      .pipe(
+        through.obj((chunk, enc, cb) => {
+          let file = JSON.parse(chunk.contents.toString('utf-8'));
+
+          file['BLOG'] = file['BLOG'].sort(
+            (a, b) =>
+              new Date(b.entity_status['date']) -
+              new Date(a.entity_status['date'])
+          );
+
+          const fileString = JSON.stringify(file);
+          const fileBuffer = Buffer.from(fileString, 'utf-8');
+
+          chunk.contents = fileBuffer;
+
+          cb(null, chunk);
+        })
+      )
+      .pipe(gulp.dest(output))
+      .on('end', cb)
+  );
 };
 
 module.exports = datasetBuild;
