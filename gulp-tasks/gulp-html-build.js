4d075d1b36f6f8d236cd8590f22927d701a8753a
Jaroslav <cebreus@live.com>
2022-02-11 23:56:59 +0100

    fix: directory traversal

--- gulp-tasks/gulp-html-build.js
+++ gulp-tasks/gulp-html-build.js
@@ -87,7 +87,7 @@ const buildHtml = (params) => {
       // Add access to site configuration
       .pipe(
         data(function () {
-          let file = params.siteConfig;
+          let file = params.siteConfig.replace(/\\/g, '/');
           file = {
             SITE: {
               ...JSON.parse(fs.readFileSync(file)),
@@ -99,7 +99,7 @@ const buildHtml = (params) => {
       // Add access to all blog posts
       .pipe(
         data(function () {
-          let file = params.datasetBlog;
+          let file = params.datasetBlog.replace(/\\/g, '/');
           file = {
             ...file,
             ...JSON.parse(fs.readFileSync(file)),
